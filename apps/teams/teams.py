7c2348a4085c3f914c324ac24a7ff1093a488741
Andrew Dant <andrewdant17@gmail.com>
2022-08-10 18:00:23 -0400

    update teams context to not be Linux specific

--- apps/teams/teams.py
+++ apps/teams/teams.py
@@ -3,8 +3,6 @@ from talon import Module
 mod = Module()
 apps = mod.apps
 apps.microsoft_teams = """
-os: linux
-and app.name: /teams/
-os: linux
-and app.name: /Teams/
+app.name: /teams/
+app.name: /Teams/
 """
